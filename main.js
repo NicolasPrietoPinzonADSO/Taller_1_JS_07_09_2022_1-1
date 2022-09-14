e6f4c705afbfa34490c6e6485eaf9c66850da0ab
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2022-09-14 11:07:12 -0500

    Ejercicio 22

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 addEventListener("DOMContentLoaded", ()=>{
-   // 21. Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución,
-    // por favor utilizar funciones matemáticas de Python para ello
-    // d = rc((x2-x1)^2 + (y2-y1)^2)
-
-    let rc = Number(prompt(`Ingrese el valor de rc`));
-    let x1 = Number(prompt(`Ingrese el valor de X1`))
-    let x2 = Number(prompt(`Ingrese el valor de x2`))
-    let y1 = Number(prompt(`Ingrese el valor de y1`))
-    let y2 = Number(prompt(`Ingrese el valor de y1`))
-    let d =  rc*((x2-x1)^2 + (y2-y1)^2)
-    console.log(`El resultado es ${d}`); 
- 
+    // 22. Pablo compro 5 productos los dos primeros con un 5% de descuento y los 2 últimos con un 2% de
+    // descuento, realice un algoritmo que indique cuanto fue el valor a pagar por cada producto y el total
+    // de la compra.
+    let p1 = Number(prompt(`Ingrese el valor del primer producto`));
+    let p2 = Number(prompt(`Ingrese el valor del segundo producto`))
+    let p3 = Number(prompt(`Ingrese el valor del tercer producto`))
+    let p4 = Number(prompt(`Ingrese el valor del cuarto producto`))
+    let p5 = Number(prompt(`Ingrese el valor del quinto producto`))
+    let totaP1 =  p1-(p1*0.05)
+    let totaP2 =  p2-(p2*0.05)
+    let totaP4 =  p4-(p4*0.02)
+    let totaP5 =  p5-(p5*0.02)
+    let total = (totaP1+totaP2+p3+totaP4+totaP5)
+    console.log(`El precio final del primer proucto es de:  ${totaP1}`);
+    console.log(`El precio final del segundo proucto es de:  ${totaP2}`);
+    console.log(`El precio final del tercer proucto es de:  ${p3}`);
+    console.log(`El precio final del cuarto proucto es de:  ${totaP4}`);
+    console.log(`El precio final del quinto proucto es de:  ${totaP5}`);
+    console.log(`El valor final de la compra es de:  ${total}`);
 })
