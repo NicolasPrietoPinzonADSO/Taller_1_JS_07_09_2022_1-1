6e0b3f91e38cd0aff35dab8f338ee100dec14982
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2022-09-21 10:36:00 -0500

    Ejercicio 28

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 7. tres estudiantes reunieron tapas de gaseosas para repartirlas y venderlas al final del año. Martin toma
-    // 2/3 del total. Jairo un cuarto del total, y Lorena se queda con el resto. ¿qué parte le corresponde a
-    // Lorena?    
-   let martin = fraction(2/3)
-   let jairo = (1/4)
-   let suma = (martin+jairo)
-   let lorena = (1/1)-suma
-   console.log(`Jairo y martin tienen un total de ${suma}`);
-   console.log(`A lorena le corresponde un total de ${lorena}`);
+    //     8. Pinturas “La brocha gorda” requiere determinar cuánto cobrar por trabajos de pintura. Considere que
+    // se cobra por m2 y realice el código que representen el algoritmo que le permita ir generando
+    // presupuestos para cada cliente.  
+       let m2 = Number(prompt(`Ingrese el espacio a pintar en m2`))
+       let precio = Number(prompt(`Ingrese el precio de m2`))
+       let total = m2*precio
+       console.log(`El total del costo del trabajo es de: ${total} COP`);
+    
+    })
 
-})
