44d25c0ff7ea8e965157e2fbb0196199dd6fa0b1
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2022-09-21 12:01:00 -0500

    Ejercicio 30

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
 addEventListener("DOMContentLoaded", () => {
-    //9. Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo,
-    //pero cuando se les realiza la entrevista sólo se les pregunta el año en que nacieron. Realice el código
-    //que representen el algoritmo para solucionar este problema.
-    //Al final debe entregar el nombre de la persona entrevistada en mayúscula sostenida y minunscula
-    //sostenida y la edad del entrevistado.
-    let obj = {}, edad;
-    do{
-        edad = Number(prompt("Ingrese su año de nacimiento"));
-        obj[prompt("Ingrese su nombre")] = 2022 - edad;
-    }
-    while (confirm("Desea insertar a otra persona?"))
-    console.log(obj);
-
+//     10. Ingrese una frase que lo identifique como programador de Software y luego mostar esta frase
+// invertida.
+    let frase = prompt('Ingrese la frase a invertir')
+    console.log(frase);
+    let invertirFrase = frase.split("").reverse().join("");
+    console.log(invertirFrase);
 })
 
