ca7e362bcc22a6680079a824e1b45880d33ad500
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2022-09-21 11:52:53 -0500

    Ejercicio 29

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-addEventListener("DOMContentLoaded", ()=>{
-    //     8. Pinturas “La brocha gorda” requiere determinar cuánto cobrar por trabajos de pintura. Considere que
-    // se cobra por m2 y realice el código que representen el algoritmo que le permita ir generando
-    // presupuestos para cada cliente.  
-       let m2 = Number(prompt(`Ingrese el espacio a pintar en m2`))
-       let precio = Number(prompt(`Ingrese el precio de m2`))
-       let total = m2*precio
-       console.log(`El total del costo del trabajo es de: ${total} COP`);
-    
-    })
+addEventListener("DOMContentLoaded", () => {
+    //9. Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo,
+    //pero cuando se les realiza la entrevista sólo se les pregunta el año en que nacieron. Realice el código
+    //que representen el algoritmo para solucionar este problema.
+    //Al final debe entregar el nombre de la persona entrevistada en mayúscula sostenida y minunscula
+    //sostenida y la edad del entrevistado.
+    let obj = {}, edad;
+    do{
+        edad = Number(prompt("Ingrese su año de nacimiento"));
+        obj[prompt("Ingrese su nombre")] = 2022 - edad;
+    }
+    while (confirm("Desea insertar a otra persona?"))
+    console.log(obj);
+
+})
 
