8dda4d5a3c1732b97209d8e22d1a95d39c86ca5a
NicolasPrietoPinzonADSO <nicolasprietopinzon5@gmail.com>
2022-09-21 08:46:28 -0500

    Ejercicio 24

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 23. Calcular la edad de una madre en el momento de dio a luz a alguno de sus hijos.
-    // • Generar el algoritmo que dé como resultado el tiempo estimado para el llenado de un
-    // depósito.
-    // • Q=V/t, siendo Q (caudal), V (volumen) y t (tiempo).
-    // • Volumen = PI *(radio^2)* H (altura del depósito)
-    // • El tiempo se encuentra en minutos.
-    // • Normalmente se mide el volumen en litros y el tiempo en segundos.
-    let radio = Number(prompt(`Ingrese el valor del radio`));
-    let altura = Number(prompt(`Ingrese el valor de la altura`))
-    let tiempo = Number(prompt(`Ingrese el valor del tiempo`))
-    let volumen = (Math.PI)*(radio**2)*altura
-    let caudal = volumen*tiempo
-    console.log(`El valor del caudal  ${caudal}`);
+    // 4. Un jefe de obra con el dinero que tiene compra cinco juegos de llaves hexagonales cada uno por
+    // $11500. Una bomba en $1168000 y tres cajas de pernos cada uno por $87000. Después de pagar le
+    // sobran $91000. ¿cuánto dinero tenía?
+    
+    let hex = 11500
+    let bomba = 1168000
+    let caja = 87000
+    let sobrante = 91000
+    let v_hex = hex*5
+    let v_caja = caja*3
+    let total = sobrante+v_caja+v_hex+bomba
+    console.log(`el total de dinero que tenia es de:  ${total}`);
 })
